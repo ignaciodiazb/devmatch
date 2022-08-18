22d6a045e42d58e4e8d3912ee62c83508584765b
Ignacio Díaz <ignacio.db@icodesx.com>
2022-08-18 19:02:09 -0400

    Refactor navbar elements into individual components

---
+++ components/CreateButton.tsx
@@ -0,0 +1,5 @@
+import { Create } from '@atlaskit/atlassian-navigation';
+
+const CreateButton = () => <Create href="/signup" text="Create account" />;
+
+export default CreateButton;
---
+++ components/CustomHome.tsx
@@ -0,0 +1,16 @@
+import { CustomProductHome } from '@atlaskit/atlassian-navigation';
+
+const CustomHome = () => (
+  <CustomProductHome
+    href="/"
+    iconAlt={'DM'}
+    iconUrl={
+      'https://cdn-icons.flaticon.com/png/512/1442/premium/1442632.png?token=exp=1660856458~hmac=2dffa710d062a4fe2b412e31ce1a745a'
+    }
+    logoAlt={'DevMatch'}
+    logoUrl={'https://i.ibb.co/JRh87FW/logo-1.png'}
+    logoMaxWidth={300}
+  />
+);
+
+export default CustomHome;
